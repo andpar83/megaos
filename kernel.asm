071d16abbfd8d522ccedef49ae8a8d02636b30f4
Andrey Parfenov <andpar83@gmail.com>
2016-12-28 07:48:34 -0800

    Added keyboard support

--- kernel.asm
+++ kernel.asm
@@ -3,6 +3,8 @@
 ;nasm directive - 32 bit
 bits 32
 section .text
+
+code_begin:
         ;multiboot spec
         align 4
         dd 0x1BADB002            ;magic
@@ -10,12 +12,43 @@ section .text
         dd - (0x1BADB002 + 0x00) ;checksum. m+f+c should be zero
 
 global start
+global read_port
+global write_port
+global keyboard_handler
+global load_idt
+
 extern kmain	        ;kmain is defined in the c file
+extern keyboard_handler_main
+
+; param1 - port number (int32)
+read_port:
+	mov edx, [esp + 4]
+	in al, dx	
+	ret
+
+; param1 - port number (int32)
+; param2 - byte of data (passed as int32)
+write_port:
+	mov   edx, [esp + 4]    
+	mov   al, [esp + 4 + 4]  
+	out   dx, al  
+	ret
+
+; param1 - address of IDT descriptor (int32)
+load_idt:
+	mov edx, [esp + 4]
+	lidt [edx]
+	sti
+	ret
+
+; keyboard interrupt handler
+keyboard_handler:                 
+	call    keyboard_handler_main
+	iretd
 
 start:
-  mov eax, start        ;getting address of our program in memory (eip)
-
   cli 			;block interrupts
+  mov eax, code_begin   ;getting address of our program in memory (eip)
   mov esp, stack_space	;set stack pointer
 
   push stack_space
@@ -23,8 +56,9 @@ start:
   call kmain
   add esp, 8
 
-
+loop:
   hlt		 	;halt the CPU
+  jmp loop
 
 section .bss
 resb 8192		;8KB for stack
